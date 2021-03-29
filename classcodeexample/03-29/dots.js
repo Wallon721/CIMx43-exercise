80aff43d78872039cc42db5ce614a348398f46ac
Wallon721 <tonytonyho721@gmail.com>
2021-03-29 18:44:08 -0400

    changes

--- classcodeexample/03-29/dots.js
+++ classcodeexample/03-29/dots.js
@@ -1,5 +1,8 @@
 //WE have 2 dots. I want to click on the second dot. When I click on it, I trasfer the dot_active to the second dot Click dot2, it becomes red.
 
 //  1. get the list of dots
+//let allDots = document.getElementsByTagName('li')
+let allDots = document.getElementsByClassName('dot')
+
 //  2. add an event listener: when you click on a dot, run a function makeActive
-//  3.makeActive function-- moves
+//  3.makeActive function-- moves the class
